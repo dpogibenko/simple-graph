7d7d41edf111bebcb3d3b80ee0c33fc788a21a24
d.pogibenko <pogibenko@gmail.com>
2019-06-29 23:58:44 +0300

    BFS implementation

---
+++ src/main/java/net/pogibenko/simplegraph/Pair.java
@@ -0,0 +1,21 @@
+package net.pogibenko.simplegraph;
+
+public class Pair<T,S> {
+
+    private T first;
+
+    private S second;
+
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
